f69461363b23515bc6c41083dbc6b2d46fa8b230
RigilKentaurus78 <109463274+RigilKentaurus78@users.noreply.github.com>
2023-08-30 21:59:43 +1000

    ported gitlab files

---
+++ 2020 config/xmonad.hs
@@ -0,0 +1,82 @@
+import XMonad
+import XMonad.Config.Desktop
+import Data.Monoid
+import Data.Maybe (isJust)
+import System.IO (hPutStrLn)
+import System.Exit (exitSuccess)
+import qualified XMonad.StackSet as W
+
+    -- Utilities
+import XMonad.Util.Loggers
+import XMonad.Util.EZConfig (additionalKeysP, additionalMouseBindings)  
+import XMonad.Util.NamedScratchpad
+import XMonad.Util.Run (safeSpawn, unsafeSpawn, runInTerm, spawnPipe)
+import XMonad.Util.SpawnOnce
+
+    -- Hooks
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, defaultPP, wrap, pad, xmobarPP, xmobarColor, shorten, PP(..))
+import XMonad.Hooks.ManageDocks (avoidStruts, docksStartupHook, manageDocks, ToggleStruts(..))
+import XMonad.Hooks.ManageHelpers (isFullscreen, isDialog,  doFullFloat, doCenterFloat) 
+import XMonad.Hooks.Place (placeHook, withGaps, smart)
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.EwmhDesktops   -- required for xcomposite in obs to work
+
+    -- Actions
+import XMonad.Actions.Minimize (minimizeWindow)
+import XMonad.Actions.Promote
+import XMonad.Actions.RotSlaves (rotSlavesDown, rotAllDown)
+import XMonad.Actions.CopyWindow (kill1, copyToAll, killAllOtherCopies, runOrCopy)
+import XMonad.Actions.WindowGo (runOrRaise, raiseMaybe)
+import XMonad.Actions.WithAll (sinkAll, killAll)
+import XMonad.Actions.CycleWS (moveTo, shiftTo, WSType(..), nextScreen, prevScreen, shiftNextScreen, shiftPrevScreen)
+import XMonad.Actions.GridSelect
+import XMonad.Actions.DynamicWorkspaces (addWorkspacePrompt, removeEmptyWorkspace)
+import XMonad.Actions.MouseResize
+import qualified XMonad.Actions.ConstrainedResize as Sqr
+
+    -- Layouts modifiers
+import XMonad.Layout.PerWorkspace (onWorkspace) 
+import XMonad.Layout.Renamed (renamed, Rename(CutWordsLeft, Replace))
+import XMonad.Layout.WorkspaceDir
+import XMonad.Layout.Spacing (spacing) 
+import XMonad.Layout.NoBorders
+import XMonad.Layout.LimitWindows (limitWindows, increaseLimit, decreaseLimit)
+import XMonad.Layout.WindowArranger (windowArrange, WindowArrangerMsg(..))
+import XMonad.Layout.Reflect (reflectVert, reflectHoriz, REFLECTX(..), REFLECTY(..))
+import XMonad.Layout.MultiToggle (mkToggle, single, EOT(EOT), Toggle(..), (??))
+import XMonad.Layout.MultiToggle.Instances (StdTransformers(NBFULL, MIRROR, NOBORDERS))
+import qualified XMonad.Layout.ToggleLayouts as T (toggleLayouts, ToggleLayout(Toggle))
+import XMonad.Layout.Gaps
+
+    -- Layouts
+import XMonad.Layout.GridVariants (Grid(Grid))
+import XMonad.Layout.SimplestFloat
+import XMonad.Layout.OneBig
+import XMonad.Layout.ThreeColumns
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.ZoomRow (zoomRow, zoomIn, zoomOut, zoomReset, ZoomMessage(ZoomFullToggle))
+import XMonad.Layout.IM (withIM, Property(Role))
+
+import qualified DBus as D
+import qualified DBus.Client as D
+import qualified Codec.Binary.UTF8.String as UTF8
+
+main = xmonad $ docks $ ewmh def 
+      { modMask = mod1Mask 
+      , terminal = "kitty"
+      , borderWidth = 2
+      , manageHook = manageDocks <+> manageHook defaultConfig
+      , layoutHook = myLayoutHook
+      , handleEventHook = handleEventHook def <+> fullscreenEventHook
+      , normalBorderColor  = "#292d3e"
+      , focusedBorderColor = "#f00"
+      }
+
+myLayoutHook = avoidStruts $ mouseResize $ windowArrange $ T.toggleLayouts floats $ 
+               mkToggle (NBFULL ?? NOBORDERS ?? EOT) $ myDefaultLayout
+             where 
+                 myDefaultLayout = tall ||| noBorders monocle ||| floats 
+
+tall     = renamed [Replace "tall"]     $ limitWindows 12 $ spacing 6 $ ResizableTall 1 (3/100) (1/2) []
+monocle  = renamed [Replace "monocle"]  $ limitWindows 20 $ Full
+floats   = renamed [Replace "floats"]   $ limitWindows 20 $ simplestFloat
